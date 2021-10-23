6d2e4d4cee83c74095215a04874099fbfa7c7466
HakanKorkz <hakan.hizmet.54.gs@gmail.com>
2021-10-23 18:57:34 +0300

    İlk ders izlendi

--- src/app.js
+++ src/app.js
@@ -5,3 +5,11 @@ let DolarBugun=9.30;
     let DolarDun=9.11;
 }
 console.log(DolarDun)
+
+let users=['Hakan KORKMAZ','Hakan ÇALIŞKAN']
+
+console.log('<ul>')
+for (let i = 0; i < users.length; i++) {
+    console.log('<li>'+users[i] +'</li>')
+}
+console.log('</ul>')
