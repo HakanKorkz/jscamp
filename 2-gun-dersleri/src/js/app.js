4f7e29688c3c7c9c0a912fb83d3cc54ce8a48a7d
hakan <hakan.hizmet.54.gs@gmail.com>
2021-11-08 20:51:05 +0300

    2. kamp  dersleri yeni bir klasöre taşındı

---
+++ 2-gun-dersleri/src/js/app.js
@@ -0,0 +1,19 @@
+/*
+let DolarDun=9.20;
+//DolarDun='Sakarya';
+let DolarBugun=9.30;
+{
+    let DolarDun=9.11;
+}
+console.log(DolarDun)
+
+let users=['Hakan KORKMAZ','Hakan ÇALIŞKAN']
+
+console.log('<ul>')
+console.log('test')
+for (let i = 0; i < users.length; i++) {
+    console.log('<li>'+users[i] +'</li>')
+}
+console.log('</ul>')
+
+*/
